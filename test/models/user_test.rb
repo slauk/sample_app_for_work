654902bf4d08f91c32093e3de07e95c6fe021332
Yana <yana.slauk21@gmail.com>
2022-04-16 17:25:21 +0300

    Add minimal standarts to password

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -20,6 +20,11 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  test "password should be present" do 
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
   test "name shouldn't be too long" do 
     @user.name = "a" * 51
     assert_not @user.valid?
@@ -59,4 +64,8 @@ class UserTest < ActiveSupport::TestCase
     assert_equal mixed_case_email.downcase, @user.reload.email
   end
 
+  test "password should have minimum length" do 
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
 end
