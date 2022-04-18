77e83f4c894aa7050e2167a81ccbb539f6034446
Yana <yana.slauk21@gmail.com>
2022-04-18 14:18:43 +0300

    Add Log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,9 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      #redirect to user page
+    if user&.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
 
     else
       flash.now[:danger] = "Invalid email or password combination!"
@@ -14,5 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 end
