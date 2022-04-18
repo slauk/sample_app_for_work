dc7cbded94c51138a2f1052cd7f63bb3b49b4a3d
Yana <yana.slauk21@gmail.com>
2022-04-18 23:59:45 +0300

    Add cookies(begin)

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,8 +6,14 @@ module SessionsHelper
 
 
 	def current_user()
-		if session[:user_id]
+		if (user_id = session[:user_id])
 			@current_user ||= User.find_by(id: session[:user_id])
+		elsif (user_id = cookies.encrypted[:user_id])
+			user = User.find_by(id: user_id)
+			if user && user.authenticated?(cookies[:remember_token])
+				log_in user
+				@current_user = user
+			end			
 		end
 	end
 
@@ -18,5 +24,11 @@ module SessionsHelper
 
 	def logged_in?
 		!current_user.nil?
+	end
+
+	def remember(user)
+		user.remember
+		cookies.permanent.encrypted[:user_id] = user_id
+		cookies.permanent[:remember_token] = user.remember_token
 	end
 end
