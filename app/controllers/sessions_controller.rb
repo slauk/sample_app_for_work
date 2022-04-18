1ec7c83edf6faf839e208366ae64f0ff38c396ee
Yana <yana.slauk21@gmail.com>
2022-04-18 19:49:22 +0300

    Fix bugs in login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,9 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user&.authenticate(params[:session][:password])
+    if user&& user.authenticate(params[:session][:password])
       log_in user
       redirect_to user
-
     else
       flash.now[:danger] = "Invalid email or password combination!"
       render 'new'
