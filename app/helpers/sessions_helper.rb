9320d82935b73faf71b4aa8f3e536e263c89f09e
Yana <yana.slauk21@gmail.com>
2022-04-22 17:20:42 +0300

    Add checkbox

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,18 +17,27 @@ module SessionsHelper
 		end
 	end
 
-	def log_out 
-		session.delete(:user_id)
-		@current_user = nil
-	end
-
+	
 	def logged_in?
 		!current_user.nil?
 	end
 
 	def remember(user)
 		user.remember
-		cookies.permanent.encrypted[:user_id] = user_id
+		cookies.permanent.encrypted[:user_id] = user.id
 		cookies.permanent[:remember_token] = user.remember_token
 	end
+
+	def forget(user)
+		user.forget
+		cookies.delete(:user_id)
+		cookies.delete(:remember_token)
+	end
+
+	def log_out 
+		forget (current_user)
+		session.delete(:user_id)
+		@current_user = nil
+	end
+
 end
