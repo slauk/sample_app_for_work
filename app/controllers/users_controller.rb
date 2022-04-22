1aff694a7801d3fce5a3afd5de898ad33c56e4ec
Yana <yana.slauk21@gmail.com>
2022-04-22 23:55:02 +0300

    Now you can change profile!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Profile was updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end 
 
   private
   def user_params
