ef1c4d3ac0b310e0c570fad54e13c4c8342b8d52
Yana <yana.slauk21@gmail.com>
2022-05-05 16:29:22 +0300

    Probably change API for sender

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,7 +5,7 @@ class Micropost < ApplicationRecord
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum:140 }
 
-  validates :image, content_type: { in: %w[image/jpeg, image/png image/gif], message: "must be valid image format"},
+  validates :image, content_type: { in: %w[image/jpeg image/jpg image/png image/gif], message: "must be valid image format"},
     size: {less_than: 5.megabytes, message: "should be less than 5MB"}
 
   def display_image
